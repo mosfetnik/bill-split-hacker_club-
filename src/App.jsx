f522a120693a0dba4acc181a2bc421ec1a914216
mosfetNik <adityarai424@gmail.com>
2024-06-21 20:53:54 +0530

    adding the event handler in the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialFriends = [
   {
     id: 118836,
@@ -28,6 +30,7 @@ export default function App() {
           <FormAddfriend />
           <Button>Add Friend</Button>
         </div>
+        <FormSplitBill />
       </div>
     </>
   );
@@ -46,6 +49,7 @@ function FriendList() {
     </>
   );
 }
+
 function Friend({ friend }) {
   return (
     <li>
@@ -75,27 +79,71 @@ function Friend({ friend }) {
   );
 }
 
-
-function Button({children}){
-
-
-  return (
-    <button className="button">{children}</button>
-  )
+function Button({ children }) {
+  return <button className="button">{children}</button>;
 }
+
 function FormAddfriend() {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("https://i.pravatar.cc/48");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const id = crypto.randomUUID();
+    const newFriend = {
+      id,
+      name,
+      image: `${image}?=${id}`,
+      balance: 0,
+    };
+
+    console.log(newFriend);
+  }
   return (
     <>
-      <form className="form-add-friend">
+      <form className="form-add-friend" onSubmit={handleSubmit}>
         <label>👩🏻‍🤝‍🧑🏻Friend Name </label>
-        <input type="text" />
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
 
-        <label >🦘 Image URL</label>
-        <button className="button">Select</button>
+        <label>🦘 Image URL</label>
+        <input
+          type="text"
+          value={image}
+          onChange={(e) => setImage(e.target.value)}
+        />
 
+        <Button>Add</Button>
       </form>
     </>
   );
 }
 
-function (FormSpliteBill)
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2> Split the Bill with X</h2>
+
+      <label> 💰 Bill Value</label>
+      <input type="text" />
+
+      <label> 👬 Ypur Expences</label>
+      <input type="text" />
+
+      <label> 💰 Xs Expences</label>
+      <input type="text" disabled />
+
+      <label> Who are paying the bill </label>
+      <select>
+        <option value="user"> You</option>
+        <option value="friend">X</option>
+      </select>
+
+      <button> Splite Bill </button>
+    </form>
+  );
+}
